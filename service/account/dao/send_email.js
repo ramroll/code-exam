823c84f313092c81ec5853a8c795ee3f975d929f
weimeng <ramroll@gmail.com>
2018-11-03 18:23:28 +0800

    邮箱验证bufix

--- service/account/dao/send_email.js
+++ service/account/dao/send_email.js
@@ -42,23 +42,12 @@ const TPL_REG = (activation_code) => {
   return `
         <p>您好！</p>
         <p>&nbsp;&nbsp;您的算法课程帐号需要激活。</p>
-        <p>&nbsp;&nbsp;请去<a href="http://www.weavinghorse.test/activation?code=${activation_code}">点击此处激活</a></p>
+        <p>&nbsp;&nbsp;请去<a href="http://www.weavinghorse.com/activation?code=${activation_code}">点击此处激活</a></p>
         <p>谢谢！</p>
 `
 }
-
-const TPL_FORGET = (activation_code) => {
-  return `
-        <p>您好！</p>
-        <p>&nbsp;&nbsp;您的在（<a http://www.weavinghorse.com>Weaving Horse程序之路</a>)申请了找回密码。</p>
-        <p>&nbsp;&nbsp;请前往Weaving Horse完成密码重置:<a href="http://www.weavinghorse.com/reset?code=${activation_code}">点击此处前往重置</a></p>
-        <p>谢谢！</p>
-`
-}
-
 
 module.exports = {
   send,
-  TPL_REG,
-  TPL_FORGET
+  TPL_REG
 }
