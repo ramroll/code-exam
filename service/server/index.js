b4f530c4bca30dbc67e1569bb051f4cc0e02fa7a
weimeng <ramroll@gmail.com>
2018-11-17 23:20:40 +0800

    增加inspire应用

--- service/server/index.js
+++ service/server/index.js
@@ -1,6 +1,9 @@
 const path = require('path')
 const fs = require('fs')
 function register(app) {
+  app.get('/inspire/*', function(req, res){
+    res.sendFile(path.resolve(__dirname, '../../dist/inspire/index.html'))
+  })
   app.get('*', function(req, res) {
     res.sendFile(path.resolve(__dirname, '../../dist/exam/index.html'))
   })
