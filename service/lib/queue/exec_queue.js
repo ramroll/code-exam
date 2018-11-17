b27111de863258a55abb6b634d1a305d738fa093
weimeng <ramroll@gmail.com>
2018-11-17 23:14:41 +0800

    bugifx

--- service/lib/queue/exec_queue.js
+++ service/lib/queue/exec_queue.js
@@ -5,13 +5,12 @@ class ExecQueue{
 
   constructor(){
 
-    if(!client) {
+    if (!client) {
       client = redis.createClient({
-        host : process.env.REDIS_HOST || 'localhost',
-        port : process.env.REDIS_PORT || 6379
-        password : process.env.REDIS_PASSWORD || undefined,
-        no_ready_check : true
-
+        host: process.env.REDIS_HOST || 'localhost',
+        port: process.env.REDIS_PORT || 6379,
+        password: process.env.REDIS_PASSWORD || undefined,
+        no_ready_check: true
       })
     }
 
