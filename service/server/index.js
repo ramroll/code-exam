d3e69def5b7e500fae7365bc9be0969c798fd6ae
Ramroll Wei <ramrollwei@RamrolldeMacBook-Pro.local>
2018-11-20 20:10:21 +0800

    bugfix

--- service/server/index.js
+++ service/server/index.js
@@ -1,10 +1,10 @@
 const path = require('path')
 const fs = require('fs')
 function register(app) {
-  app.get('/account/*', function (req, res) {
+  app.get(/\/account/, function (req, res) {
     res.sendFile(path.resolve(__dirname, '../../dist/account/index.html'))
   })
-  app.get('/inspire/*', function(req, res){
+  app.get(/\/inspire/, function(req, res){
     res.sendFile(path.resolve(__dirname, '../../dist/inspire/index.html'))
   })
   app.get('*', function(req, res) {
