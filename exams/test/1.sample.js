36ea4d90d14325df089f06619a7de73a0b903ed0
weimeng <ramroll@gmail.com>
2018-12-11 09:19:28 +0800

    去掉无用的文件

--- exams/test/1.sample.js
+++
@@ -1,3 +0,0 @@
-function sum(n) {
-  /// TODO
-}
--- exams/test/scores.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = [{
-  exe_time: 100,
-  weight : 10,
-  min_score : 5,
-}, {
-  exe_time: 200000,
-  weight : 90,
-  min_score : 30
-}]
