e3279b6b2aede403e5c631f28130d6a9ab7bcb0a
weimeng <ramroll@gmail.com>
2018-12-22 15:05:25 +0800

    dynamic import with home

--- app/exam/index.js
+++ app/exam/index.js
@@ -1,13 +1,10 @@
 import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import Exam from './Exam'
+// import Exam from './Exam'
 import Home from './Home'
-import ExamExplain from './ExamExplain'
-import ExamExplainEdit from './ExamExplainEdit'
-import ExamExplains from './ExamExplains'
 
-import ExamAnswer from './Answer'
+
 import './styl/index.styl'
 
 
@@ -15,6 +12,39 @@ import Header from '../common/component/Header'
 
 import withMe from '../common/component/withMe'
 
+const Loading = () => <span>Loading...</span>
+
+
+function withDynamicImport(load) {
+
+  class DynamicImport extends Component {
+    state = {
+      component: null
+    }
+    componentDidMount() {
+      load()
+        .then((component) => {
+          this.setState(() => ({
+            component : component.default
+          }))
+        })
+    }
+    render() {
+      if (!this.state.component) {
+        return <Loading />
+      }
+      return <this.state.component {...this.props} />
+    }
+  }
+  return DynamicImport
+
+}
+
+const Exam = withDynamicImport(()=>import('./Exam'))
+const ExamExplain = withDynamicImport(()=>import('./ExamExplain'))
+const ExamExplainEdit = withDynamicImport(()=>import('./ExamExplainEdit'))
+const ExamAnswer = withDynamicImport(()=>import('./Answer'))
+const ExamExplains = withDynamicImport(()=>import('./ExamExplains'))
 @withMe()
 class App extends Component {
 
